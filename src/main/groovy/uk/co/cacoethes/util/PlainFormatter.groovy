7781a46c97fbc87f99fd0fa96caed1352c79dd86
Tommy Barker <thomas.p.barker@gmail.com>
2013-06-09 21:02:14 -0400

    cleanup PlainFormatter

--- src/main/groovy/uk/co/cacoethes/util/PlainFormatter.groovy
+++ src/main/groovy/uk/co/cacoethes/util/PlainFormatter.groovy
@@ -1,22 +1,24 @@
 package uk.co.cacoethes.util
 
-import java.util.logging.*
+import java.util.logging.Formatter
+import java.util.logging.LogRecord
 
 /**
  * This is a java.util.logging formatter that simply prints the log messages
  * as-is, without any decoration. It basically turns log statements into
- * simple {@code println()}s. But, you have the advantage of log levels!
+ * simple {@code println ( )}s. But, you have the advantage of log levels!
  */
 @groovy.transform.CompileStatic
 class PlainFormatter extends Formatter {
     String format(LogRecord record) {
         def message = record.message + '\n'
-        if(record.thrown) {
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            pw.println();
-            record.getThrown().printStackTrace(pw);
-            pw.close();
+        //copied from SimpleFormatter
+        if (record.thrown) {
+            StringWriter sw = new StringWriter()
+            PrintWriter pw = new PrintWriter(sw)
+            pw.println()
+            record.getThrown().printStackTrace(pw)
+            pw.close()
             message += sw.toString()
         }
 
