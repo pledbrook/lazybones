dac558ac6250a637dda9e17c9ff84c3e93b64c5c
Peter Ledbrook <peter@cacoethes.co.uk>
2015-06-25 11:34:53 +0100

    Add --subs option to `list` command.
    Further work to #147. There is now a `--subs` option on the `list` command that
    only displays the subtemplates installed in the current Lazybones-created
    project. The command errors out if there is no .lazybones directory in the
    current working directory.
    
    I also modified the `--cached` option to group templates by name, so that all
    available versions are listed as one entry.

--- lazybones-app/src/integTest/groovy/uk/co/cacoethes/lazybones/ListFunctionalSpec.groovy
+++ lazybones-app/src/integTest/groovy/uk/co/cacoethes/lazybones/ListFunctionalSpec.groovy
@@ -7,6 +7,7 @@ import org.junit.*
 
 class ListFunctionalSpec extends AbstractFunctionalSpec {
     @Rule Recorder recorder = new Recorder()
+    def remoteTemplates = ["afterburnerfx", "dropwizard", "gaelyk", "gradle-plugin", "gradle-quickstart"]
 
     void setup() {
         initProxy(recorder.proxy.address())
@@ -14,9 +15,6 @@ class ListFunctionalSpec extends AbstractFunctionalSpec {
 
     @Betamax(tape='list-tape')
     def "list command prints all available packages"() {
-        given: "An expected list of packages"
-        def expected = ["afterburnerfx", "dropwizard", "gaelyk", "gradle-plugin", "gradle-quickstart"]
-
         when: "I run lazybones with the list command"
         def exitCode = runCommand(["list"], baseWorkDir)
 
@@ -25,22 +23,16 @@ class ListFunctionalSpec extends AbstractFunctionalSpec {
         output =~ /(?m)^Available mappings\s+/ +
                 /\s+customRatpack  -> http:\/\/dl.dropboxusercontent.com\/u\/29802534\/custom-ratpack.zip\s+/ +
                 /\s+doesNotExist   -> file:\/\/\/does\/not\/exist\s+/
-        output =~ /\s+${expected.join('\\s+')}\s+/
+        output =~ /\s+${remoteTemplates.join('\\s+')}\s+/
         !(output =~ /Exception/)
         !(output =~ /Cached templates/)
+        !(output =~ /Available subtemplates/)
     }
 
     @Betamax(tape='list-tape')
     def "list command prints cached templates"() {
-        given: "A list of remote packages"
-        def remoteTmpls = ["afterburnerfx", "dropwizard", "gaelyk", "gradle-plugin", "gradle-quickstart"]
-
-        and: "A template in the cache matching a remote one"
-        def ratpackPackage = new File(cacheDirPath, "ratpack-0.1.zip")
-        if (!ratpackPackage.exists()) {
-            FileUtils.touch(ratpackPackage)
-            filesToDelete << ratpackPackage
-        }
+        given: "A template in the cache matching a remote one"
+        def versions = addMultiVersionCachedPackage("ratpack")
 
         when: "I run the list command with --cached option"
         def exitCode = runCommand(["list", "--cached"], baseWorkDir)
@@ -53,60 +45,97 @@ class ListFunctionalSpec extends AbstractFunctionalSpec {
 
         output =~ /(?m)^Cached templates\s+/ +
                 /Oops-stuff                    1.0.4\s+/ +
-                /ratpack                       0.1\s+/ +
+                /ratpack                       ${versions.join(', ')}\s+/ +
                 /subtemplates-tmpl             0.1\s+/ +
                 /test-handlebars               0.1.1/
         !(output =~ /Exception/)
-        !(remoteTmpls.any { it in output })
+        !(remoteTemplates.any { it in output })
+        !(output =~ /Available subtemplates/)
     }
 
     @Betamax(tape='list-tape')
-    def "list command prints no sub-templates"() {
-        given: "A lazybones generated project"
-        def projectDir = new File(baseWorkDir, 'project')
-        def localLazybonesDir = new File(projectDir, '.lazybones')
-        localLazybonesDir.mkdirs()
+    def "list command with --subs in non-project directory"() {
+        when: "I run the list command with --subs option"
+        // do not clear workdir or we lose our setup
+        def exitCode = runCommand(["list", "--subs"], baseWorkDir, [], false)
 
-        filesToDelete << localLazybonesDir
-        filesToDelete << projectDir
-
-        when: "I run the list command with --cached option"
-        // do not clear workdir or we loose our setup
-        def exitCode = runCommand(["list", "--cached"], projectDir, [], false)
-
-        then: "It does not display any available sub-templates"
-        exitCode == 0
-        !(output =~ /(?m)^Available sub-templates\s+/)
+        then: "It displays an error message"
+        exitCode != 0
+        output =~ /You can only use --subs in a Lazybones-created project directory/
+        !(output =~ /(?m)^Available subtemplates\s+/)
         !(output =~ /Exception/)
+        !(output =~ /Cached templates/)
+        !(output =~ /Available mappings/)
+        !(remoteTemplates.any { it in output })
     }
 
     @Betamax(tape='list-tape')
-    def "list command prints available sub-templates"() {
+    def "list command prints no subtemplates"() {
         given: "A lazybones generated project"
-        def projectDir = new File(baseWorkDir, 'project')
-        def localLazybonesDir = new File(projectDir, '.lazybones')
-        localLazybonesDir.mkdirs()
+        def projectDir = createTempProject()
 
-        and: "A sub-template is available in the local lazybones directory"
-        def templateFile = new File(localLazybonesDir, 'artifact-template-1.0.0.zip')
-        templateFile.text = '''
-        Any content will do. We check for the file's existence
-        and its filename pattern only.
-        '''
+        when: "I run the list command with --subs option"
+        // do not clear workdir or we lose our setup
+        def exitCode = runCommand(["list", "--subs"], projectDir, [], false)
 
-        filesToDelete << templateFile
-        filesToDelete << localLazybonesDir
-        filesToDelete << projectDir
-
-        when: "I run the list command with --cached option"
-        // do not clear workdir or we loose our setup
-        def exitCode = runCommand(["list", "--cached"], projectDir, [], false)
-
-        then: "It does display all available sub-templates"
+        then: "It does not display any available subtemplates"
         exitCode == 0
-        output =~ /(?m)^Available sub-templates\s+/ +
-                  /\s+artifact\s+/
-
+        output =~ /This project has no subtemplates/
+        !(output =~ /(?m)^Available subtemplates\s+/)
         !(output =~ /Exception/)
+        !(output =~ /Cached templates/)
+        !(output =~ /Available mappings/)
+        !(remoteTemplates.any { it in output })
+    }
+
+    @Betamax(tape='list-tape')
+    def "list command prints available subtemplates"() {
+        given: "A lazybones generated project"
+        def projectDir = createTempProject()
+
+        and: "A subtemplate is available in the local lazybones directory"
+        addSubtemplate(projectDir, "artifact", "1.0.0")
+        addSubtemplate(projectDir, "web", "0.8")
+
+        when: "I run the list command with --subs option"
+        // do not clear workdir or we loose our setup
+        def exitCode = runCommand(["list", "--subs"], projectDir, [], false)
+
+        then: "It does display all available subtemplates"
+        exitCode == 0
+        output =~ /(?m)^Available subtemplates\s+/ +
+                  /^\s+artifact\s+1.0.0\s+/ +
+                  /^\s+web\s+0.8\s+/
+        !(output =~ /Exception/)
+        !(output =~ /Cached templates/)
+        !(output =~ /Available mappings/)
+        !(remoteTemplates.any { it in output })
+    }
+
+    private File createTempProject() {
+        final projectDir = new File(baseWorkDir, 'project')
+        new File(projectDir, '.lazybones').mkdirs()
+
+        filesToDelete << projectDir
+        return projectDir
+    }
+
+    private List addMultiVersionCachedPackage(String pkgName) {
+        def versions = ["0.9.18", "0.1"]
+        for (String version in versions) {
+            def pkgFile = new File(cacheDirPath, "${pkgName}-${version}.zip")
+            if (!pkgFile.exists()) {
+                FileUtils.touch(pkgFile)
+                filesToDelete << pkgFile
+            }
+        }
+
+        return versions
+    }
+
+    private File addSubtemplate(File projectDir, String name, String version) {
+        final templateFile = new File(new File(projectDir, ".lazybones"), "${name}-template-${version}.zip")
+        FileUtils.touch(templateFile)
+        return templateFile
     }
 }
--- lazybones-app/src/main/groovy/uk/co/cacoethes/lazybones/commands/ListCommand.groovy
+++ lazybones-app/src/main/groovy/uk/co/cacoethes/lazybones/commands/ListCommand.groovy
@@ -22,7 +22,10 @@ USAGE: list
 
     private static final String INDENT = "    "
     private static final String CACHED_OPTION = "cached"
+    private static final String SUBTEMPLATES_OPTION = "subs"
     private static final int PADDING = 30
+
+    private static final String VERSION_PATTERN = /\d+\.\d[^-]*(?:-SNAPSHOT)?/
 
     File cacheDir
 
@@ -52,20 +55,20 @@ USAGE: list
         def remoteTemplates = fetchRemoteTemplates(config.getSetting("bintrayRepositories"))
 
         boolean offline = false
-        if (!optionSet.has(CACHED_OPTION)) {
+        if (!optionSet.hasOptions()) {
             offline = handleRemoteTemplates(remoteTemplates, globalOptions.stacktrace as boolean)
         }
-        if (optionSet.has(CACHED_OPTION) || offline) {
-            handleCachedTemplates(cacheDir)
-        }
-        handleMappings(config.getSubSettings("templates.mappings"))
-        handleSubTemplates()
+
+        if (optionSet.has(CACHED_OPTION) || offline) handleCachedTemplates(cacheDir)
+        if (!optionSet.has(SUBTEMPLATES_OPTION)) handleMappings(config.getSubSettings("templates.mappings"))
+        if (optionSet.has(SUBTEMPLATES_OPTION)) return handleSubtemplates()
 
         return 0
     }
 
     protected OptionParser doAddToParser(OptionParser parser) {
         parser.accepts(CACHED_OPTION, "Lists the cached templates instead of the remote ones.")
+        parser.accepts(SUBTEMPLATES_OPTION, "Lists any subtemplates in the current project.")
         return parser
     }
 
@@ -86,13 +89,17 @@ USAGE: list
         println "Cached templates"
         println()
 
-        def templateNamePattern = ~/^(.*)-(\d[^-]*(?:-SNAPSHOT)?)\.zip$/
+        def templateNamePattern = ~/^(.*)-($VERSION_PATTERN)\.zip$/
 
-        def templates = findMatchingTemplates(cacheDir, templateNamePattern)
+        def templates = findMatchingTemplates(cacheDir, templateNamePattern).groupBy { File f ->
+            templateNamePattern.matcher(f.name)[0][1]
+        }.collectEntries { String tmplName, List<File> files ->
+            // Extract the version numbers and make those the key value.
+            [ tmplName, files.collect { templateNamePattern.matcher(it.name)[0][2] } ]
+        }
 
-        for (f in templates) {
-            def matcher = templateNamePattern.matcher(f.name)
-            println INDENT + matcher[0][1].padRight(PADDING) + matcher[0][2]
+        for (entry in templates) {
+            println INDENT + entry.key.padRight(PADDING) + entry.value.sort().reverse().join(", ")
         }
 
         println()
@@ -158,29 +165,36 @@ USAGE: list
     }
 
     /**
-     * Handle local sub-templates only
+     * Handle local subtemplates only
      */
-    protected static void handleSubTemplates() {
+    protected static int handleSubtemplates() {
         // is the current dir a project created by Lazybones?
         File lazybonesDir = new File('.lazybones')
-        if (lazybonesDir.exists()) {
-            def templateNamePattern = ~/^(.*)-template-(\d[^-]*(?:-SNAPSHOT)?)\.zip$/
-
-            def templates = findMatchingTemplates(lazybonesDir, templateNamePattern)
-
-            // are there any templates available?
-            if (templates) {
-                println "Available sub-templates"
-                println()
-
-                for (f in templates) {
-                    def matcher = templateNamePattern.matcher(f.name)
-                    println INDENT + matcher[0][1].padRight(PADDING)
-                }
-
-                println()
-            }
+        if (!lazybonesDir.exists()) {
+            println "You can only use --subs in a Lazybones-created project directory"
+            return 1   // Error exit code
         }
+
+        def templateNamePattern = ~/^(.*)-template-($VERSION_PATTERN)\.zip$/
+        def templates = findMatchingTemplates(lazybonesDir, templateNamePattern)
+
+        // are there any templates available?
+        if (templates) {
+            println "Available subtemplates"
+            println()
+
+            for (f in templates) {
+                def matcher = templateNamePattern.matcher(f.name)
+                println INDENT + matcher[0][1].padRight(PADDING) + matcher[0][2]
+            }
+
+            println()
+        }
+        else {
+            println "This project has no subtemplates"
+        }
+
+        return 0    // 'No error' exit code
     }
 
     private static File[] findMatchingTemplates(File dir, Pattern pattern) {
