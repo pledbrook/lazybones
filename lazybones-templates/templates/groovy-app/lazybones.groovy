0dcbce8bd20ad6d9aa5da8574c85800096272663
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-07-30 16:25:14 -0500

    bumping groovy-app stuff

--- lazybones-templates/templates/groovy-app/lazybones.groovy
+++ lazybones-templates/templates/groovy-app/lazybones.groovy
@@ -1,4 +1,4 @@
-def filterProperties = [:]
+Map filterProperties = [:]
 filterProperties.group = ask("Define value for 'group': ", "group")
 filterProperties.version = ask("Define value for 'version' [0.1]: ", "version", "0.1")
 getString()
