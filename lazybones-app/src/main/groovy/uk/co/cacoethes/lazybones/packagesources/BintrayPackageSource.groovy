a375dfb3a6af2fded0ef6c8cf0862f04371bd034
Peter Ledbrook <peter@cacoethes.co.uk>
2016-03-02 11:55:06 +0000

    Upgrade to Groovy WSLite to 1.1.2.
    This version has builtin support for the standard JVM proxy properties, so there
    is no longer any need to manually configure the proxy. I've also fixed a nasty
    hack that disabled SSL certificate verification if a proxy was being used. This
    now only happens during integration tests, or more specifically if an
    'integration.test' system property is set to the string `"true"`.

--- lazybones-app/src/main/groovy/uk/co/cacoethes/lazybones/packagesources/BintrayPackageSource.groovy
+++ lazybones-app/src/main/groovy/uk/co/cacoethes/lazybones/packagesources/BintrayPackageSource.groovy
@@ -24,13 +24,7 @@ class BintrayPackageSource implements PackageSource {
     BintrayPackageSource(String repositoryName) {
         repoName = repositoryName
         restClient = new RESTClient(API_BASE_URL)
-
-        // For testing with Betamax: set up a proxy if required. groovyws-lite
-        // doesn't currently support the http(s).proxyHost and http(s).proxyPort
-        // system properties, so we have to manually create the proxy ourselves.
-        def proxy = loadSystemProxy(false)
-        if (proxy)  {
-            restClient.httpClient.proxy = proxy
+        if (System.getProperty("integration.test") == "true") {
             restClient.httpClient.sslTrustAllCerts = true
         }
     }
@@ -94,23 +88,5 @@ class BintrayPackageSource implements PackageSource {
         }
 
         return pkgInfo
-    }
-
-    /**
-     * Reads the proxy information from the {@code http(s).proxyHost} and {@code http(s).proxyPort}
-     * system properties if set and returns a {@code java.net.Proxy} instance configured with
-     * those settings. If the {@code proxyHost} setting has no value, then this method returns
-     * {@code null}.
-     * @param useHttpsProxy {@code true} if you want the HTTPS proxy, otherwise {@code false}.
-     */
-    private Proxy loadSystemProxy(boolean useHttpsProxy) {
-        def propertyPrefix = useHttpsProxy ? "https" : "http"
-        def proxyHost = System.getProperty("${propertyPrefix}.proxyHost")
-        if (!proxyHost) return null
-
-        def proxyPort = System.getProperty("${propertyPrefix}.proxyPort")?.toInteger()
-        proxyPort = proxyPort ?: (useHttpsProxy ? 443 : 80)
-
-        return new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort))
     }
 }
